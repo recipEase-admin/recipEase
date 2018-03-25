7d9241ab469e7bc73117467d64c73871f799c0dd
RobertMatyjek <rmatyjek@ufl.edu>
2018-03-25 15:32:25 -0400

    Updated Functionality

--- RecipEase/app/src/main/java/com/recipease/project/DrawerActivity.java
+++ RecipEase/app/src/main/java/com/recipease/project/DrawerActivity.java
@@ -1,66 +1,83 @@
 package com.recipease.project;
 
 import android.content.Intent;
-import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.Window;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 
-import java.util.ArrayList;
+public class DrawerActivity extends AppCompatActivity {
 
-public class BaseActivity extends FragmentActivity {
-
-    protected DrawerLayout mDrawer;
-
+    protected DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.base_layout);
+        setContentView(R.layout.activity_drawer);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.activity_drawer);
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+       // getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        mDrawerList = (ListView)findViewById(R.id.navList);
 
-        String[] osArray = { "Profile", "Create A Recipe", "About", "Logout" };
-        mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, osArray);
-        mDrawerList.setAdapter(mAdapter);
-        mDrawerList.bringToFront();
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        // set item as selected to persist highlight
+                        menuItem.setChecked(true);
+                        // close drawer when item is tapped
+                        mDrawerLayout.closeDrawers();
 
-        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView parent, View view, int position, long id) {
-                switch (position) {
-                    case 0:
-                        Intent goToProfile = new Intent(HomeActivity.this, ProfileActivity.class);
-                        startActivity(goToProfile);
-                        break;
-                    case 1:
-                        Intent goToCreateRecipe = new Intent(HomeActivity.this, CreateRecipeActivity.class);
-                        startActivity(goToCreateRecipe);
-                        break;
-                    case 2:
-                        Intent goToAbout = new Intent ( HomeActivity.this, AboutActivity.class);
-                        startActivity(goToAbout);
-                        break;
-                    case 3: //Logout
-                        FirebaseAuth.getInstance().signOut();
-                        Toast.makeText(HomeActivity.this, "Succesfully logged out", Toast.LENGTH_LONG).show();
-                        Intent i = new Intent(HomeActivity.this, MainActivity.class);
-                        startActivity(i);
-                        break;
-                }
-            }
-        });
+                        switch(menuItem.getItemId()){
+                            case R.id.profile:
+                                Intent intent=new Intent(DrawerActivity.this,ProfileActivity.class);
+                                startActivity(intent);
+                                break;
+                            case R.id.home:
+                                Intent intentA=new Intent(DrawerActivity.this,HomeActivity.class);
+                                startActivity(intentA);
+                                break;
+                            case R.id.create_recipe:
+                                Intent intentb = new Intent(DrawerActivity.this, CreateRecipeActivity.class);
+                                startActivity(intentb);
+                                break;
+                            case R.id.find_recipe:
+                                Intent intentc = new Intent(DrawerActivity.this, IngredientSelector.class);
+                                startActivity(intentc);
+                                break;
+                            case R.id.about:
+                               Intent intentd = new Intent(DrawerActivity.this, AboutActivity.class);
+                                startActivity(intentd);
+                                break;
+                            case R.id.logout:
+                                FirebaseAuth.getInstance().signOut();
+                                Toast.makeText(DrawerActivity.this, "Succesfully logged out", Toast.LENGTH_LONG).show();
+                                Intent i = new Intent(DrawerActivity.this, MainActivity.class);
+                                startActivity(i);
+                                break;
+
+
+                        }
+                        return true;
+                }}
+        );
+
+
+}
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        if(mToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
-
 }
