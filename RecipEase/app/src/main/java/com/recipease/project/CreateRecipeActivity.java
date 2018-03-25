794ca21af9b8956bcbd5a8fb0582d5e0a752303f
Andrew Ratz <ar4757@gmail.com>
2018-03-25 19:12:13 -0400

    Tied new navbar into personal recipes
    New navbar works with personal recipes and new create recipes with ingredients

--- RecipEase/app/src/main/java/com/recipease/project/CreateRecipeActivity.java
+++ RecipEase/app/src/main/java/com/recipease/project/CreateRecipeActivity.java
@@ -15,6 +15,7 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.ArrayAdapter;
