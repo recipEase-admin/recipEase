e6a2763ec2ab3458fcdb82a8308d7ff3d2f53629
Robert Robillard <BobbyRobillard@users.noreply.github.com>
2018-04-01 18:05:08 -0400

    Interal curse word checking added
    curse words contained inside input will now be marked as profanity

--- RecipEase/app/src/main/java/com/recipease/project/Filter.java
+++ RecipEase/app/src/main/java/com/recipease/project/Filter.java
@@ -6,19 +6,28 @@ package com.recipease.project;
 
 public class Filter {
 
-    public Filter() {
+    String[] profanityWords = {
+            "fuck","shit","ass","cunt","nigger","bitch", "faggot", "asshole","goddamn",
+            "damn", "pussy"
+    };
 
-    }
+    public Filter() {}
 
     public boolean containsProfanity( String inputToCheck ) {
-        String[] profanityWords = {"fuck","shit","ass","cunt","nigger","bitch", "faggot", "asshole","goddamn","damn"};
+
         String[] splitInput = inputToCheck.split(" ");
-        for(String curseWord : profanityWords) {
-            for(String wordToCheck : splitInput) {
-                if(curseWord.equals( wordToCheck )) {
-                    return true;
-                }
+        for(String singleWord : splitInput) {
+            if(hasProfanityWordContained(singleWord)) {
+                return true;
             }
+        }
+        return false;
+    }
+
+    private boolean hasProfanityWordContained(String input) {
+        for(String curseWord : profanityWords) {
+            if(input.contains(curseWord))
+                return true;
         }
         return false;
     }
