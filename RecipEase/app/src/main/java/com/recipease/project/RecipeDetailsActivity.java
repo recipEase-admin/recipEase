389ae83f1407087be041f45dab0bdf8450db397d
Robert Robillard <BobbyRobillard@users.noreply.github.com>
2018-04-08 18:35:56 -0400

    removed useless includes

--- RecipEase/app/src/main/java/com/recipease/project/RecipeDetailsActivity.java
+++ RecipEase/app/src/main/java/com/recipease/project/RecipeDetailsActivity.java
@@ -22,16 +22,15 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
-import java.util.concurrent.CountDownLatch;
 
 import static android.content.ContentValues.TAG;
 
-import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class RecipeDetailsActivity extends DrawerActivity {
 
